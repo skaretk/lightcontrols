8c52316bf1feaed8cd9bad33364076c7108b17f8
Kjetil Skaret <kskaret@gmail.com>
2017-05-31 23:33:32 +0200

    added arduino file

---
+++ light_controller.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(PC0, OUTPUT);
+  pinMode(PC1, OUTPUT);
+  pinMode(PC2, OUTPUT);
+  pinMode(PC3, OUTPUT);
+  pinMode(PC4, OUTPUT);
+  pinMode(PC5, OUTPUT);
+  pinMode(PC6, OUTPUT);
+}
+
+void loop() {
+  // Check and enable correct output
+
+}
